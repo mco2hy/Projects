e97415f8713788205aad1f0b1e26995c2ff8e142
Can Yaralı <mco2hy@gmail.com>
2020-01-05 18:21:28 +0300

    ( 05.01.2020 ) #1

---
+++ SnakeGame/SnakeGame/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SnakeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Point> snakePoints = new List<Point>();
+        public List<Point> bonusPoints = new List<Point>();
+
+        private Brush snakeColor = Brushes.Green;
+
+        private enum SIZE
+        {
+            THIN = 4,
+            NORMAL = 6,
+            THICK = 8
+        };
+        private enum MOVINGDIRECTION
+        {
+            UPWARDS = 8,
+            DOWNWARDS = 2,
+            TOLEFT = 4,
+            TORIGHT = 6
+        };
+        private TimeSpan FAST = new TimeSpan(1);
+        private TimeSpan MODERATE = new TimeSpan(10000);
+        private TimeSpan SLOW = new TimeSpan(50000);
+        private TimeSpan DAWNSLOW = new TimeSpan(500000);
+
+        private Point startingPoint = new Point(100, 100);
+        private Point currentPosition = new Point();
+
+        private int direction = 0;
+
+        private int previousDirection = 0;
+
+        private int headSize = (int)SIZE.THICK;
+
+        private int length = 100;
+        private int score = 0;
+        private Random rnd = new Random();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+
+            timer.Interval = MODERATE;
+            timer.Start();
+
+            this.KeyDown += new KeyEventHandler(OnButtonKeyDown);
+            paintSnake(startingPoint);
+            currentPosition = startingPoint;
+
+            for (int n = 0; n < 10; n++)
+            {
+                paintBonus(n);
+            }
+
+        }
+
+        private void paintBonus(int index)
+        {
+             
+        Point bonusPoint = new Point(rnd.Next(5, 620), rnd.Next(5, 380));
+
+            Ellipse newEllipse = new Ellipse();
+            newEllipse.Fill = Brushes.Red;
+            newEllipse.Width = headSize;
+            newEllipse.Height = headSize;
+
+            Canvas.SetTop(newEllipse, currentPosition.Y);
+            Canvas.SetLeft(newEllipse, currentPosition.X);
+
+            paintCanvas.Children.Insert(index, newEllipse);
+            snakePoints.Insert(index, bonusPoint);
+
+        }
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Down:
+                    if (previousDirection != (int)MOVINGDIRECTION.UPWARDS)
+                        direction = (int)MOVINGDIRECTION.DOWNWARDS;
+                    break;
+                case Key.Up:
+                    if (previousDirection != (int)MOVINGDIRECTION.DOWNWARDS)
+                        direction = (int)MOVINGDIRECTION.UPWARDS;
+                    break;
+                case Key.Left:
+                    if (previousDirection != (int)MOVINGDIRECTION.TORIGHT)
+                        direction = (int)MOVINGDIRECTION.TOLEFT;
+                    break;
+                case Key.Right:
+                    if (previousDirection != (int)MOVINGDIRECTION.TOLEFT)
+                        direction = (int)MOVINGDIRECTION.TORIGHT;
+                    break;
+            }
+            previousDirection = direction;
+        }
+
+        private void paintSnake(Point currentPosition)
+        {
+            
+
+            Ellipse newEllipse = new Ellipse();
+            newEllipse.Fill = Brushes.Green;
+            newEllipse.Width = headSize;
+            newEllipse.Height = headSize;
+
+            Canvas.SetTop(newEllipse, currentPosition.Y);
+            Canvas.SetLeft(newEllipse, currentPosition.X);
+
+            int count = paintCanvas.Children.Count;
+            paintCanvas.Children.Add(newEllipse);
+            snakePoints.Add(currentPosition);
+
+            if (count > length)
+            {
+                paintCanvas.Children.RemoveAt(count - length + 9);
+                snakePoints.RemoveAt(count - length);
+            }
+
+            //paintCanvas.Children.Add(newEllipse);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            switch (direction)
+            {
+                case (int)MOVINGDIRECTION.DOWNWARDS:
+                    currentPosition.Y += 1;
+                    paintSnake(currentPosition);
+                    break;
+                case (int)MOVINGDIRECTION.UPWARDS:
+                    currentPosition.Y -= 1;
+                    paintSnake(currentPosition);
+                    break;
+                case (int)MOVINGDIRECTION.TOLEFT:
+                    currentPosition.X -= 1;
+                    paintSnake(currentPosition);
+                    break;
+                case (int)MOVINGDIRECTION.TORIGHT:
+                    currentPosition.X += 1;
+                    paintSnake(currentPosition);
+                    break;  
+            }
+
+            if ((currentPosition.X <5) || (currentPosition.X > 620)|| (currentPosition.Y < 5) || (currentPosition.Y > 380))
+                GameOver();
+            int n = 0;
+            foreach (Point point in bonusPoints)
+            {
+
+                if ((Math.Abs(point.X - currentPosition.X) < headSize) &&
+                    (Math.Abs(point.Y - currentPosition.Y) < headSize))
+                {
+                    length += 10;
+                    score += 10;
+
+                    bonusPoints.RemoveAt(n);
+                    paintCanvas.Children.RemoveAt(n);
+                    paintBonus(n);
+                    break;
+                }
+                n++;
+            }
+
+            for (int q = 0; q < (snakePoints.Count - headSize*2); q++)
+            {
+                Point point = new Point(snakePoints[q].X, snakePoints[q].Y);
+                if ((Math.Abs(point.X - currentPosition.X) < (headSize)) && (Math.Abs(point.Y - currentPosition.Y) < (headSize)))
+                {
+                    GameOver();
+                    break;
+                }
+            }
+
+        }
+        private void GameOver()
+        {
+            MessageBox.Show("Kaybettin. Skor : " + score.ToString(), " Oyun Bitti " , MessageBoxButton.OK, MessageBoxImage.Hand);
+            this.Close();
+        }
+        
+
+        
+        
+    }
+    
+}
+
